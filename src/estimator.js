46030984c79f401dfe171d6cee8d924480d231f6
chubbystrings <martinsokwor@hotmail.com>
2020-04-08 09:19:58 +0100

    challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,30 @@
-const covid19ImpactEstimator = (data) => data;
+const estimateCurrentlyInfected = (data) => {
+  const cases = data;
+  const impact = {};
+  const severeImpact = {};
+  const impactEstimate = 10;
+  const severeImapctEstimate = 50;
+
+  // impact
+  impact.currentlyInfected = cases.reportedCases * impactEstimate;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** impactEstimate);
+
+  // severeImpact
+  severeImpact.currentlyInfected = cases.reportedCases * severeImapctEstimate;
+  // eslint-disable-next-line max-len
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** severeImapctEstimate);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
+const covid19ImpactEstimator = (data) => {
+  const estimate = estimateCurrentlyInfected(data);
+  return estimate;
+};
+
 
 export default covid19ImpactEstimator;
